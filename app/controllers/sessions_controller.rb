47237f3a85924d5fd46372f9b5fb61f07257b207
trishogen <patriciaogen@gmail.com>
2019-12-11 21:27:15 -0500

    logs out users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:user][:email])
     session[:user_id] = @user.id
-    redirect_to '/'
+    redirect_to root_path
+  end
+
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to login_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'application#index'
+
+  #log in and log out
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :runs
   resources :routes
