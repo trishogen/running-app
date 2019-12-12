c7393ccfae1943c9c55e0c2280f39745f91a7c05
trishogen <patriciaogen@gmail.com>
2019-12-11 20:54:33 -0500

    signup new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
-  has_secure_password
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :runs
   resources :routes
-  resources :users
+   resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
