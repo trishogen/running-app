1d40962e8dbf4c0b83b90634a87528dcc76cdd3a
trishogen <patriciaogen@gmail.com>
2019-12-14 14:21:55 -0500

    create new running routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,17 @@
 class RoutesController < ApplicationController
+
+  def new
+    @route = Route.new
+  end
+
+  def create
+    @route = Route.create(route_params)
+    redirect_to route_path(@route)
+  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:title, :location, :distance, :elevation)
+  end
 end
