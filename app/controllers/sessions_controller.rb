371a26b96aabb57d6f0d49540396bae40ee4d252
trishogen <patriciaogen@gmail.com>
2019-12-16 21:49:38 -0500

    user is authenticated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:user][:email])
+    return head(:forbidden) unless params[:password] == @user.password
     session[:user_id] = @user.id
     redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true, format: { with:
     /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-  validates_confirmation_of :password
+
+  validates :password, confirmation: true, length: { minimum: 8 }
+
   validates :first_name, presence: true, format: { without: /[0-9]/,
     message: "does not allow numbers" }
 
