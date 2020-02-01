b788a0e918a7cc31846e7af94ea8aad5e4069195
trishogen <patriciaogen@gmail.com>
2020-01-31 20:53:05 -0500

    confirm password is correct

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       end
     else
       @user = User.find_by(email: params[:user][:email])
-      return head(:forbidden) unless params[:password] == @user.password
+      return head(:forbidden) unless @user.authenticate(params[:user][:password])
     end
 
     session[:user_id] = @user.id
